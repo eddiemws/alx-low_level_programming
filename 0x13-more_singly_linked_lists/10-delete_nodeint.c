556faf714acd1eb8d8d6f223214af2f56064f93d
eddy <106661971+eddiemws@users.noreply.github.com>
2024-04-09 20:27:44 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
         free(current);
         return (1);
     }
-
     previous = *head;
     current = (*head)->next;
 
@@ -22,10 +21,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
         previous = current;
         current = current->next;
     }
-
     if (current == NULL)
         return (-1);
-
     previous->next = current->next;
     free(current);
     return (1);
