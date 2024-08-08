a98623f5ca4f46243d3783ae19b6be8537833fa3
eddy <106661971+eddiemws@users.noreply.github.com>
2024-08-08 22:30:00 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,16 +7,7 @@
  * @s2: string to compare
  * Return: 1 if identical, 0 if not
  */
-int wildcmp(char *s1, char *s2)
-{
-	if (!*s1 && !*s2)
-		return (1);
-	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	return (0);
-}
+
 
 /*
 * int wildcmp(char *s1, char *s2)
