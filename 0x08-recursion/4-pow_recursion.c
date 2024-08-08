9856b96dff27729feed8d6e0d5eaab463372cf64
eddy <106661971+eddiemws@users.noreply.github.com>
2024-08-08 22:32:12 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,4 +7,11 @@
  *
  * Return: x raised to the power of y
  */
-
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
