d5edb6d5c5f84888531926a9ec13ef023dc25b66
eddy <106661971+eddiemws@users.noreply.github.com>
2024-08-08 22:30:50 +0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,11 +5,4 @@
  * @n: number to be factored
  * Return: factorial of n
  */
-int factorial(int n)
-{
-	if (n < 0)
-		return (-1);
-	if (n == 0)
-		return (1);
-	return (n * factorial(n - 1));
-}
+
