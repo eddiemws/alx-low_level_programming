4e1763477912faca2b10a5cfb5ec19b9392a9e39
eddy <106661971+eddiemws@users.noreply.github.com>
2024-08-18 23:46:40 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,21 +5,4 @@
  * Description: prints numbers
  * Return: Always 0 (success)
  */
-int main(void)
-{
-	int i;
 
-	for (i = '0'; i <= '9' ; i++)
-	{
-		putchar(i);
-
-		if (i != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
-	putchar('\n');
-	return (0);
-}
